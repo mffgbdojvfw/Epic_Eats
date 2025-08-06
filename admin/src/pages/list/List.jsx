169278e57407ac0261ecfbd48d0e75852dc9fa10
Maan3112 <136999624+Maan3112@users.noreply.github.com>
2025-08-06 18:49:06 +0530

    Updated API endpoints and replaced localhost with Render URL

--- admin/src/pages/list/List.jsx
+++ admin/src/pages/list/List.jsx
@@ -1,91 +1,188 @@
-import React, { useState } from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import axios from 'axios'
-import { toast } from "react-toastify"
-import { useEffect } from 'react'
-import { assets } from '../../assets/assets'
-import "./list.css"
+// import React, { useState } from 'react'
+// import Navbar from '../../components/navbar/Navbar'
+// import Sidebar from '../../components/sidebar/Sidebar'
+// import axios from 'axios'
+// import { toast } from "react-toastify"
+// import { useEffect } from 'react'
+// import { assets } from '../../assets/assets'
+// import "./list.css"
+
+// const List = () => {
+//   const [list, setlist] = useState([])
+//   const url = "http://localhost:4300"
+
+//   const fetchlist = async () => {
+//     const response = await axios.get(`${url}/api/food/list`)
+//     try {
+//       if (response.data.success) {
+//         setlist(response.data.data)
+//         console.log(response.data.data)
+//       }
+//       else {
+//         toast.error("Error")
+//       }
+//     } catch (err) {
+//       toast.error(err)
+//     }
+//   }
+
+
+//   useEffect(() => {
+//     fetchlist();
+//   }, []);
+
+//   const removelist = async(foodId) =>{
+// const response = await axios.post(`${url}/api/food/remove`,{id:foodId})
+// await fetchlist();
+// if(response.data.success){
+//   toast.success(response.data.message)
+// }
+// else{
+//   toast.error("Error")
+// }
+//   }
+
+//   return (
+//     <>
+//     <div className='navbar'>
+//       <Navbar />
+//       </div>
+//       {/* <hr className='hr'/> */}
+//       <div className="all">
+//         <Sidebar className="sidebar"/>
+//         <div className="list">
+//           <p className='all-food-list'>All Food List</p>
+//           <div className="food-lists">
+//             <div className="food-lists-heading">
+//               <b className='list-img'>Image</b>
+//               <b className='name-heading'>Name</b>
+//               <b>Category</b>
+//               <b>Price</b>
+//               <b>Action</b>
+//             </div>
+//             <hr />
+//           {
+//             list.map((item,index)=>{
+//               // console.log(`Item:`, item);
+//               // const imageUrl = `${url}/images/${item.image}`;
+//               // console.log(`Image URL: ${imageUrl}`);
+//               return(
+//               <div key={index} className="list-items">
+//               <img  className="list-img" src= {`${url}/images/${item.image}`} alt="" />
+             
+//               <p className='item-names'>{item.name}</p>
+//               <p>{item.category}</p>
+//               <p className='prise'>${item.price}</p>
+//               <div className='remove-list-cont'>
+//               <img className='list-remove' src={assets.remove_icon_red} onClick={()=>removelist(item._id)} alt="" />
+//               </div>
+//               </div>
+//               )
+//             })
+
+//           }
+//           </div>
+//         </div>
+//       </div>
+//       </>
+//       )
+// }
+
+//       export default List
+
+
+
+import React, { useState, useEffect } from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import { toast } from "react-toastify";
+import { assets } from '../../assets/assets';
+import API from '../../api'; // ✅ Using centralized API config
+import "./list.css";
 
 const List = () => {
-  const [list, setlist] = useState([])
-  const url = "http://localhost:4300"
+  const [list, setList] = useState([]);
 
-  const fetchlist = async () => {
-    const response = await axios.get(`${url}/api/food/list`)
+  // Fetch food list
+  const fetchList = async () => {
     try {
+      const response = await API.get('/api/food/list');
       if (response.data.success) {
-        setlist(response.data.data)
-        console.log(response.data.data)
-      }
-      else {
-        toast.error("Error")
+        setList(response.data.data);
+        console.log('Fetched food list:', response.data.data);
+      } else {
+        toast.error("Failed to fetch list");
       }
     } catch (err) {
-      toast.error(err)
+      console.error(err);
+      toast.error("Network error while fetching list");
     }
-  }
+  };
 
+  // Remove food item
+  const removeListItem = async (foodId) => {
+    try {
+      const response = await API.post('/api/food/remove', { id: foodId });
+      if (response.data.success) {
+        toast.success(response.data.message);
+        fetchList(); // Refresh the list
+      } else {
+        toast.error("Failed to remove item");
+      }
+    } catch (err) {
+      console.error(err);
+      toast.error("Network error while removing item");
+    }
+  };
 
   useEffect(() => {
-    fetchlist();
+    fetchList();
   }, []);
-
-  const removelist = async(foodId) =>{
-const response = await axios.post(`${url}/api/food/remove`,{id:foodId})
-await fetchlist();
-if(response.data.success){
-  toast.success(response.data.message)
-}
-else{
-  toast.error("Error")
-}
-  }
 
   return (
     <>
-    <div className='navbar'>
-      <Navbar />
+      <div className="navbar">
+        <Navbar />
       </div>
-      {/* <hr className='hr'/> */}
+
       <div className="all">
-        <Sidebar className="sidebar"/>
+        <Sidebar className="sidebar" />
         <div className="list">
-          <p className='all-food-list'>All Food List</p>
+          <p className="all-food-list">All Food List</p>
           <div className="food-lists">
             <div className="food-lists-heading">
-              <b className='list-img'>Image</b>
-              <b className='name-heading'>Name</b>
+              <b className="list-img">Image</b>
+              <b className="name-heading">Name</b>
               <b>Category</b>
               <b>Price</b>
               <b>Action</b>
             </div>
             <hr />
-          {
-            list.map((item,index)=>{
-              // console.log(`Item:`, item);
-              // const imageUrl = `${url}/images/${item.image}`;
-              // console.log(`Image URL: ${imageUrl}`);
-              return(
+            {list.map((item, index) => (
               <div key={index} className="list-items">
-              <img  className="list-img" src= {`${url}/images/${item.image}`} alt="" />
-             
-              <p className='item-names'>{item.name}</p>
-              <p>{item.category}</p>
-              <p className='prise'>${item.price}</p>
-              <div className='remove-list-cont'>
-              <img className='list-remove' src={assets.remove_icon_red} onClick={()=>removelist(item._id)} alt="" />
+                <img
+                  className="list-img"
+                  src={`${process.env.REACT_APP_API_URL || 'https://epic-eats-backend.onrender.com'}/images/${item.image}`}
+                  alt={item.name}
+                />
+                <p className="item-names">{item.name}</p>
+                <p>{item.category}</p>
+                <p className="prise">${item.price}</p>
+                <div className="remove-list-cont">
+                  <img
+                    className="list-remove"
+                    src={assets.remove_icon_red}
+                    onClick={() => removeListItem(item._id)}
+                    alt="Remove"
+                  />
+                </div>
               </div>
-              </div>
-              )
-            })
-
-          }
+            ))}
           </div>
         </div>
       </div>
-      </>
-      )
-}
+    </>
+  );
+};
 
-      export default List
+export default List;
--- admin/src/pages/order/Order.jsx
+++ admin/src/pages/order/Order.jsx
@@ -1,105 +1,230 @@
-import React from 'react'
-import Navbar from '../../components/navbar/Navbar'
-import Sidebar from '../../components/sidebar/Sidebar'
-import "./order.css"
-import { useState } from 'react'
-import axios from "axios"
-import {toast} from "react-toastify"
-import img from "./parcel_icon.png"
-import { useEffect } from 'react'
-const Order = () => {
-const[data,setdata] = useState([])
-const url = "http://localhost:4300"
+// import React from 'react'
+// import Navbar from '../../components/navbar/Navbar'
+// import Sidebar from '../../components/sidebar/Sidebar'
+// import "./order.css"
+// import { useState } from 'react'
+// import axios from "axios"
+// import {toast} from "react-toastify"
+// import img from "./parcel_icon.png"
+// import { useEffect } from 'react'
+// import API from "../../api"; // Adjust path if needed
+
+// const Order = () => {
+// const[data,setdata] = useState([])
+// const url = "http://localhost:4300"
 
 
-const fetchorders = async() =>{
+// const fetchorders = async() =>{
   
-  const response = await axios.get(`${url}/api/order/list`) 
-  if(response.data.success){
-    setdata(response.data.data)
-    console.log(response.data.data)
-  }
-  else{
-    toast.error("Error")
-  }
-  }
+//   const response = await axios.get(`${url}/api/order/list`) 
+//   if(response.data.success){
+//     setdata(response.data.data)
+//     console.log(response.data.data)
+//   }
+//   else{
+//     toast.error("Error")
+//   }
+//   }
 
-  const StatusHandler = async(event,orderId) =>{
-    const response = await axios.post(`${url}/api/order/status`,{
-      orderId,
-      status:event.target.value
-    })
-    if(response.data.success){
-      await fetchorders()
+//   const StatusHandler = async(event,orderId) =>{
+//     const response = await axios.post(`${url}/api/order/status`,{
+//       orderId,
+//       status:event.target.value
+//     })
+//     if(response.data.success){
+//       await fetchorders()
+//     }
+//   }
+
+//   useEffect(()=>{
+//     fetchorders()
+//   },[])
+
+//   useEffect(() => {
+//     console.log('Updated data:', data); // Log the updated state data
+//   }, [data]);
+
+//   return (
+//     <>
+//     <div className='navbar'>
+//         <Navbar/>
+//     </div>
+//     <div className="all">
+//     <Sidebar className="sidebar"/>
+//     <div className="my-orders">
+//         <h2>My Orders</h2>
+//         <div className="container3">
+//             {data.map((order,index)=>{
+//                 return(
+//                     // <OrderItem key={order._id}  order={order}/>
+//                     <div key={index} className="my-orders-order">
+//                         <img src={img} alt=''/>
+//                       <div className='all-info'>
+//                       <p>{order.items.map((item,index)=>{
+//                         if(index === order.items.length-1){
+//                             return item.item+" x "+item.quantity
+//                         }
+//                         else{
+//                             return item.item+" x "+item.quantity+", "
+//                         }
+//                       })}</p>  
+//                          {order.address && order.address.length > 0 && order.address[0] ? (
+//                   <>
+//                   <div className='address'>
+//                   <p className='order-item-name'>{order.address[0].firstname}</p>
+//                   <p className='order-item-street'>{order.address[0].address+", "+order.address[0].street}</p>
+//                   <p className='order-item-street'>{order.address[0].city+", "+order.address[0].state+", "+order.address[0].country+", "+order.address[0].zipcode}</p>
+//                   </div>
+//                   <p className='order-user-phone'>{order.address[0].phone}</p>
+//                   </> 
+//                 ) : (
+//                   <p className='order-item-name'>No address</p>
+//                 )}
+                
+              
+//                       </div>
+                   
+//                       <p className='same'>₹{order.amount}.00</p>
+//                       <p className='same'>items:{order.items.length}</p>
+//                       <select onChange={(event)=>StatusHandler(event,order._id)} value={order.status}  className={
+//                 order.status === 'Food Processing' ? 'processing' :
+//                 order.status === 'Out for delivery' ? 'out-for-delivery' :
+//                 order.status === 'Received' ? 'received' : ''}>
+//                         <option value="Food Processing">Food Processing</option>
+//                         <option value="Out for delivery">Out for delivery</option>
+//                         <option value="Received">Received</option>
+//                       </select>
+//                     </div>
+//                 )
+//             })}
+//         </div>
+//       </div>
+//     </div>
+//     </>
+//   )
+// }
+
+// export default Order
+
+
+
+import React, { useEffect, useState } from 'react';
+import Navbar from '../../components/navbar/Navbar';
+import Sidebar from '../../components/sidebar/Sidebar';
+import './order.css';
+import { toast } from 'react-toastify';
+import img from './parcel_icon.png';
+import API from '../../api'; // ✅ Use centralized API with Render baseURL
+
+const Order = () => {
+  const [data, setdata] = useState([]);
+
+  const fetchorders = async () => {
+    try {
+      const response = await API.get('/api/order/list');
+      if (response.data.success) {
+        setdata(response.data.data);
+        console.log(response.data.data);
+      } else {
+        toast.error('Error fetching orders');
+      }
+    } catch (error) {
+      toast.error('Network error');
+      console.error('Fetch error:', error);
     }
-  }
+  };
 
-  useEffect(()=>{
-    fetchorders()
-  },[])
+  const StatusHandler = async (event, orderId) => {
+    try {
+      const response = await API.post('/api/order/status', {
+        orderId,
+        status: event.target.value,
+      });
+      if (response.data.success) {
+        await fetchorders();
+      }
+    } catch (error) {
+      toast.error('Failed to update status');
+      console.error('Status update error:', error);
+    }
+  };
 
   useEffect(() => {
-    console.log('Updated data:', data); // Log the updated state data
+    fetchorders();
+  }, []);
+
+  useEffect(() => {
+    console.log('Updated data:', data);
   }, [data]);
 
   return (
     <>
-    <div className='navbar'>
-        <Navbar/>
-    </div>
-    <div className="all">
-    <Sidebar className="sidebar"/>
-    <div className="my-orders">
-        <h2>My Orders</h2>
-        <div className="container3">
-            {data.map((order,index)=>{
-                return(
-                    // <OrderItem key={order._id}  order={order}/>
-                    <div key={index} className="my-orders-order">
-                        <img src={img} alt=''/>
-                      <div className='all-info'>
-                      <p>{order.items.map((item,index)=>{
-                        if(index === order.items.length-1){
-                            return item.item+" x "+item.quantity
-                        }
-                        else{
-                            return item.item+" x "+item.quantity+", "
-                        }
-                      })}</p>  
-                         {order.address && order.address.length > 0 && order.address[0] ? (
-                  <>
-                  <div className='address'>
-                  <p className='order-item-name'>{order.address[0].firstname}</p>
-                  <p className='order-item-street'>{order.address[0].address+", "+order.address[0].street}</p>
-                  <p className='order-item-street'>{order.address[0].city+", "+order.address[0].state+", "+order.address[0].country+", "+order.address[0].zipcode}</p>
-                  </div>
-                  <p className='order-user-phone'>{order.address[0].phone}</p>
-                  </> 
-                ) : (
-                  <p className='order-item-name'>No address</p>
-                )}
-                
-              
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="all">
+        <Sidebar className="sidebar" />
+        <div className="my-orders">
+          <h2>My Orders</h2>
+          <div className="container3">
+            {data.map((order, index) => (
+              <div key={index} className="my-orders-order">
+                <img src={img} alt="" />
+                <div className="all-info">
+                  <p>
+                    {order.items.map((item, index) =>
+                      index === order.items.length - 1
+                        ? `${item.item} x ${item.quantity}`
+                        : `${item.item} x ${item.quantity}, `
+                    )}
+                  </p>
+
+                  {order.address && order.address.length > 0 && order.address[0] ? (
+                    <>
+                      <div className="address">
+                        <p className="order-item-name">{order.address[0].firstname}</p>
+                        <p className="order-item-street">
+                          {order.address[0].address}, {order.address[0].street}
+                        </p>
+                        <p className="order-item-street">
+                          {order.address[0].city}, {order.address[0].state},{' '}
+                          {order.address[0].country}, {order.address[0].zipcode}
+                        </p>
                       </div>
-                   
-                      <p className='same'>₹{order.amount}.00</p>
-                      <p className='same'>items:{order.items.length}</p>
-                      <select onChange={(event)=>StatusHandler(event,order._id)} value={order.status}  className={
-                order.status === 'Food Processing' ? 'processing' :
-                order.status === 'Out for delivery' ? 'out-for-delivery' :
-                order.status === 'Received' ? 'received' : ''}>
-                        <option value="Food Processing">Food Processing</option>
-                        <option value="Out for delivery">Out for delivery</option>
-                        <option value="Received">Received</option>
-                      </select>
-                    </div>
-                )
-            })}
+                      <p className="order-user-phone">{order.address[0].phone}</p>
+                    </>
+                  ) : (
+                    <p className="order-item-name">No address</p>
+                  )}
+                </div>
+
+                <p className="same">₹{order.amount}.00</p>
+                <p className="same">Items: {order.items.length}</p>
+                <select
+                  onChange={(event) => StatusHandler(event, order._id)}
+                  value={order.status}
+                  className={
+                    order.status === 'Food Processing'
+                      ? 'processing'
+                      : order.status === 'Out for delivery'
+                      ? 'out-for-delivery'
+                      : order.status === 'Received'
+                      ? 'received'
+                      : ''
+                  }
+                >
+                  <option value="Food Processing">Food Processing</option>
+                  <option value="Out for delivery">Out for delivery</option>
+                  <option value="Received">Received</option>
+                </select>
+              </div>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
     </>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
+
